220ad671e9e05cd920cadd3caa841660ad332115
Shima Safari <shimasafarimg@gmail.com>
2025-12-09 21:03:35 +0330

    Initial commit of project files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen p-10">
+        <h1 className="text-2xl font-bold mb-4">Component Library Demo</h1>
+        <div className="space-y-4 text-slate-600"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
